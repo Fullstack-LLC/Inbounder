47223546990380aca77cf0bafe2c25300fde1c17
David Church <david.church@fullstackllc.net>
2025-07-01 16:56:33 -0500

    fix: use existing templates instead of hardcoded default-template

--- src/Jobs/ProcessInboundEmailsJob.php
+++ src/Jobs/ProcessInboundEmailsJob.php
@@ -12,6 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Inbounder\Models\MailgunInboundEmail;
 use Inbounder\Models\DistributionList;
+use Inbounder\Models\EmailTemplate;
 use Carbon\Carbon;
 
 class ProcessInboundEmailsJob implements ShouldQueue
@@ -26,9 +27,29 @@ class ProcessInboundEmailsJob implements ShouldQueue
             foreach ($recipients as $recipient) {
                 $list = DistributionList::where('email_address', $recipient)->first();
                 if ($list) {
-                    // You may want to pass more variables/options here
-                    SendTemplatedEmailJob::dispatch($recipient, 'default-template', [], []);
-                    Log::info('Dispatched SendTemplatedEmailJob for recipient', ['recipient' => $recipient, 'list_id' => $list->id]);
+                    // Try to find a welcome template, fallback to newsletter if not found
+                    $template = EmailTemplate::where('slug', 'welcome')->first()
+                        ?? EmailTemplate::where('slug', 'newsletter')->first();
+
+                    if ($template) {
+                        $variables = [
+                            'name' => $recipient,
+                            'first_name' => explode('@', $recipient)[0],
+                            'app_name' => config('app.name', 'Our App'),
+                        ];
+
+                        SendTemplatedEmailJob::dispatch($recipient, $template->slug, $variables, []);
+                        Log::info('Dispatched SendTemplatedEmailJob for recipient', [
+                            'recipient' => $recipient,
+                            'list_id' => $list->id,
+                            'template' => $template->slug
+                        ]);
+                    } else {
+                        Log::warning('No suitable template found for inbound email response', [
+                            'recipient' => $recipient,
+                            'list_id' => $list->id
+                        ]);
+                    }
                 }
             }
             $email->processed_at = Carbon::now();
